aa4a184c71bdd59d0ed26b2cb841603d86592555
Ashraful Islam <rx.ju.edu@gmail.com>
2021-07-26 20:28:20 +0700

    Wireup dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -9,6 +9,7 @@ import Progress from "./components/Progress";
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -22,6 +23,7 @@ export default () => (
         <Suspense fallback={<Progress />}>
           <Switch>
             <Route path="/auth" component={AuthLazy} />
+            <Route path="/dashboard" component={DashboardLazy} />
             <Route path="/" component={MarketingLazy} />
           </Switch>
         </Suspense>
